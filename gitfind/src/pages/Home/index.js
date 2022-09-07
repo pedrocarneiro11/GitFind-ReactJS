e6a557a63cf0a1f19cebfb0385f7d9c8623186e7
Pedro <pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.com>
2022-09-07 15:48:22 -0300

    inicio da estilizacao do css

--- gitfind/src/pages/Home/index.js
+++ gitfind/src/pages/Home/index.js
@@ -1,8 +1,10 @@
-
+import {} from '../../components/Header';
+import { Header } from '../../components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
     </div>
   );
 }
