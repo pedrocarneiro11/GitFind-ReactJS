5d6fc8b0656cfde13a749d7ce9df41d61348cff4
Pedro <pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.com>
2022-09-08 10:22:31 -0300

    adicao do logo e do input

--- gitfind/src/pages/Home/index.js
+++ gitfind/src/pages/Home/index.js
@@ -1,10 +1,21 @@
 import {} from '../../components/Header';
 import { Header } from '../../components/Header';
+import logo from '../../assets/logo2.png';
+import "./styles.css"
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="conteudo">
+        <img src={logo} className="gitHubLogo" alt="GitHub Logo"></img>
+        <div className="informacoes">
+          <div>
+            <input name="usuario" placeholder="@username"></input>
+            <button>Buscar</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
